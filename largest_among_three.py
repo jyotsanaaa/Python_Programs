724a18cfb5219c059bd44ecb3f288749bce74bd2
jyotsanaaa <48643355+jyotsanaaa@users.noreply.github.com>
2020-07-23 22:15:07 +0545

    Create largest_among_three.py

---
+++ largest_among_three.py
@@ -0,0 +1,10 @@
+#Find the largest among three numbers.
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+num3 = int(input("Enter third number : "))
+if num1 > num2 and num1 > num3:
+    print(f"{num1} is the largest.")
+elif num2 > num1 and num2 > num3:
+    print(f"{num2} is the largest.")
+else:
+    print(f"{num3} is the largest.")
