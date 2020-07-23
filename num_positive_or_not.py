4df55dc5a890649831e8f70e30bcf481d7f366cb
jyotsanaaa <48643355+jyotsanaaa@users.noreply.github.com>
2020-07-23 22:12:09 +0545

    Add files via upload

---
+++ num_positive_or_not.py
@@ -0,0 +1,9 @@
+#Check whether a number is positive, negative or zero.
+
+num = float(input("Enter any number : "))
+if num > 0:
+    print("Number is positive. ")
+elif num == 0:
+    print("Number is equal to zero. ")
+else:
+    print("Number is negative. ")
